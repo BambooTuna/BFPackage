b23d9aebe96899318f601c56f8d203ca9e07cd91
Suzuki Takeo <takeo@stko.info>
2019-06-30 12:41:07 +0900

    use scala fmt

--- src/main/scala/com/github/BambooTuna/BFPackage/GetMyFXStatus.scala
+++ src/main/scala/com/github/BambooTuna/BFPackage/GetMyFXStatus.scala
@@ -1,6 +1,6 @@
 package com.github.BambooTuna.BFPackage
 
-import akka.actor.{Actor, ActorSystem}
+import akka.actor.{ Actor, ActorSystem }
 import akka.stream.ActorMaterializer
 import com.github.BambooTuna.BFPackage.GetMyFXStatus._
 import com.github.BambooTuna.CryptoLib.restAPI.client.bitflyer.APIList.BitflyerEnumDefinition
@@ -9,16 +9,16 @@ import monix.eval.Task
 import monix.execution.Scheduler.Implicits.global
 
 import scala.concurrent.duration._
-import scala.util.{Failure, Success}
+import scala.util.{ Failure, Success }
 
 import io.circe.generic.auto._
 import org.slf4j.LoggerFactory
 
 class GetMyFXStatus(api: BitflyerRestAPIs, options: GMFSOptions = GMFSOptions()) extends Actor {
 
-  implicit val system: ActorSystem = context.system
+  implicit val system: ActorSystem             = context.system
   implicit val materializer: ActorMaterializer = ActorMaterializer()
-  val logger = LoggerFactory.getLogger(getClass)
+  val logger                                   = LoggerFactory.getLogger(getClass)
 
   def receive = {
     case GetPosition =>
@@ -26,10 +26,9 @@ class GetMyFXStatus(api: BitflyerRestAPIs, options: GMFSOptions = GMFSOptions())
         case Success(value) =>
           value.fold(
             e => self ! InternalError(e.bodyString),
-            r => self ! PositionData(
-              r.map(p =>
-                convertSizeToSignedValue(p.size, p.side)
-              ).sum
+            r =>
+              self ! PositionData(
+                r.map(p => convertSizeToSignedValue(p.size, p.side)).sum
             )
           )
           Thread.sleep(options.positionInterval.toMillis)
@@ -51,10 +50,10 @@ class GetMyFXStatus(api: BitflyerRestAPIs, options: GMFSOptions = GMFSOptions())
 
   def convertSizeToSignedValue(size: BigDecimal, side: BitflyerEnumDefinition.Side): BigDecimal = {
     size *
-      (side match {
-        case BitflyerEnumDefinition.Side.Buy => 1
-        case BitflyerEnumDefinition.Side.Sell => -1
-      })
+    (side match {
+      case BitflyerEnumDefinition.Side.Buy  => 1
+      case BitflyerEnumDefinition.Side.Sell => -1
+    })
   }
 
 }
@@ -62,9 +61,9 @@ class GetMyFXStatus(api: BitflyerRestAPIs, options: GMFSOptions = GMFSOptions())
 object GetMyFXStatus {
 
   case class GMFSOptions(
-                          positionInterval: Duration = 5.seconds,
-                          errorInterval: Duration = 5.seconds
-                        )
+      positionInterval: Duration = 5.seconds,
+      errorInterval: Duration = 5.seconds
+  )
 
   sealed trait Command
   private case object GetPosition extends Command
@@ -73,7 +72,7 @@ object GetMyFXStatus {
 
   sealed trait MyStatus
   case class PositionData(size: BigDecimal) extends MyStatus
-  case class OrderData() extends MyStatus
-  case class PnlData() extends MyStatus
+  case class OrderData()                    extends MyStatus
+  case class PnlData()                      extends MyStatus
 
 }
--- src/main/scala/com/github/BambooTuna/BFPackage/StreamActor.scala
+++ src/main/scala/com/github/BambooTuna/BFPackage/StreamActor.scala
@@ -1,14 +1,19 @@
 package com.github.BambooTuna.BFPackage
 
 import akka.actor.SupervisorStrategy.Restart
-import akka.actor.{Actor, ActorSystem, OneForOneStrategy, Props}
+import akka.actor.{ Actor, ActorSystem, OneForOneStrategy, Props }
 import akka.stream.ActorMaterializer
 import com.github.BambooTuna.BFPackage.Protocol.StreamChannel
 import com.github.BambooTuna.BFPackage.Protocol.StreamChannel._
 import com.github.BambooTuna.BFPackage.StreamActor._
 import com.github.BambooTuna.CryptoLib.restAPI.client.bitflyer.APIList.BitflyerEnumDefinition
-import com.github.BambooTuna.WebSocketManager.{WebSocketManager, WebSocketOptions}
-import com.github.BambooTuna.WebSocketManager.WebSocketProtocol.{ConnectStart, ConnectedSucceeded, OnMessage, SendMessage}
+import com.github.BambooTuna.WebSocketManager.{ WebSocketManager, WebSocketOptions }
+import com.github.BambooTuna.WebSocketManager.WebSocketProtocol.{
+  ConnectStart,
+  ConnectedSucceeded,
+  OnMessage,
+  SendMessage
+}
 import org.slf4j.LoggerFactory
 
 import scala.concurrent.ExecutionContextExecutor
@@ -21,12 +26,15 @@ import io.circe.generic.auto._
 
 class StreamActor(channelName: StreamChannel, debug: Boolean = false) extends Actor {
 
-  implicit val system: ActorSystem = context.system
-  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val system: ActorSystem                        = context.system
+  implicit val materializer: ActorMaterializer            = ActorMaterializer()
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
-  val logger = LoggerFactory.getLogger(getClass)
+  val logger                                              = LoggerFactory.getLogger(getClass)
 
-  val webSocketManager = context.actorOf(Props(classOf[WebSocketManager], WebSocketOptions(host = "wss://ws.lightstream.bitflyer.com/json-rpc")), WebSocketManager.ActorName)
+  val webSocketManager = context.actorOf(
+    Props(classOf[WebSocketManager], WebSocketOptions(host = "wss://ws.lightstream.bitflyer.com/json-rpc")),
+    WebSocketManager.ActorName
+  )
 
   override def preStart() = {
     super.preStart()
@@ -44,29 +52,24 @@ class StreamActor(channelName: StreamChannel, debug: Boolean = false) extends Ac
 
       val errorMessage = for {
         json <- org
-        r <- json.hcursor.downField("error").as[String]
+        r    <- json.hcursor.downField("error").as[StreamDataError]
       } yield r
 
       val result = for {
-        json <- org
+        json        <- org
         channelName <- json.hcursor.downField("params").downField("channel").as[StreamChannel]
         r <- channelName match {
-          case Executions_FX => json.hcursor.downField("params").downField("message").as[Seq[Execution]].right.map(LightningExecutions)
-          case Executions_Spot => json.hcursor.downField("params").downField("message").as[Seq[Execution]].right.map(LightningExecutions)
-
-          case Ticker_FX => json.hcursor.downField("params").downField("message").as[LightningTicker]
-          case Ticker_Spot => json.hcursor.downField("params").downField("message").as[LightningTicker]
-
-          case Board_FX => json.hcursor.downField("params").downField("message").as[LightningBoard]
-          case Board_Spot => json.hcursor.downField("params").downField("message").as[LightningBoard]
-
-          case Board_snapshot_FX => json.hcursor.downField("params").downField("message").as[LightningBoardSnapshot]
-          case Board_snapshot_Spot => json.hcursor.downField("params").downField("message").as[LightningBoardSnapshot]
+          case Executions_FX | Executions_Spot =>
+            json.hcursor.downField("params").downField("message").as[Seq[Execution]].right.map(LightningExecutions)
+          case Ticker_FX | Ticker_Spot => json.hcursor.downField("params").downField("message").as[LightningTicker]
+          case Board_FX | Board_Spot   => json.hcursor.downField("params").downField("message").as[LightningBoard]
+          case Board_snapshot_FX | Board_snapshot_Spot =>
+            json.hcursor.downField("params").downField("message").as[LightningBoardSnapshot]
         }
       } yield r
 
       if (result.isRight) context.parent ! result.right.get
-      else if (errorMessage.isRight) context.parent ! StreamDataError(errorMessage.right.get)
+      else if (errorMessage.isRight) context.parent ! errorMessage.right.get
       else self ! InternalException(JsonParseException())
     case InternalException(e) =>
       if (debug) logger.debug(e.getMessage)
@@ -95,38 +98,37 @@ object StreamActor {
   sealed trait StreamData
 
   case class Execution(
-                                  id: Long,
-                                  side: BitflyerEnumDefinition.Side,
-                                  price: Long,
-                                  size: BigDecimal,
-                                  exec_date: String,
-                                  buy_child_order_acceptance_id: String,
-                                  sell_child_order_acceptance_id: String
-                                )
+      id: Long,
+      side: BitflyerEnumDefinition.Side,
+      price: Long,
+      size: BigDecimal,
+      exec_date: String,
+      buy_child_order_acceptance_id: String,
+      sell_child_order_acceptance_id: String
+  )
   case class LightningExecutions(executions: Seq[Execution]) extends StreamData
 
   case class LightningTicker(
-                              product_code: String,
-                              timestamp: String,
-                              tick_id: Long,
-                              best_bid: Long,
-                              best_ask: Long,
-                              best_bid_size: BigDecimal,
-                              best_ask_size: BigDecimal,
-                              total_bid_depth: BigDecimal,
-                              total_ask_depth: BigDecimal,
-                              ltp: Long,
-                              volume: BigDecimal,
-                              volume_by_product: BigDecimal
-                            ) extends StreamData
+      product_code: String,
+      timestamp: String,
+      tick_id: Long,
+      best_bid: Long,
+      best_ask: Long,
+      best_bid_size: BigDecimal,
+      best_ask_size: BigDecimal,
+      total_bid_depth: BigDecimal,
+      total_ask_depth: BigDecimal,
+      ltp: Long,
+      volume: BigDecimal,
+      volume_by_product: BigDecimal
+  ) extends StreamData
 
   case class Board(price: Long, size: BigDecimal)
-  case class LightningBoard(mid_price: Long, bids: Seq[Board], asks: Seq[Board]) extends StreamData
+  case class LightningBoard(mid_price: Long, bids: Seq[Board], asks: Seq[Board])         extends StreamData
   case class LightningBoardSnapshot(mid_price: Long, bids: Seq[Board], asks: Seq[Board]) extends StreamData
 
-  case class StreamDataError(errorMessage: String) extends StreamData
+  case class StreamDataError(code: Long, message: String, data: String) extends StreamData
 
   case class InternalException(e: Exception)
   case class JsonParseException() extends Exception()
 }
-
